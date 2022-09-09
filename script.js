be648438d4cef6504fdef72328adb1ec9e9b9686
pravinargade18 <pravinargade18@gmail.com>
2022-09-10 00:25:37 +0530

    minor changes

--- script.js
+++ script.js
@@ -74,3 +74,27 @@ const currencies = new Map([
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
+
+
+const displayMovements = function(movements) {
+
+    containerMovements.innerHTML='';
+
+    movements.forEach(function(mov,i){
+      const type=mov>0?'deposit':'withdrawal';
+
+        const html = 
+        `<div class="movements__row">
+          <div class="movements__type movements__type--${type}">${i+1} ${type}</div>
+          <div class="movements__value">${mov}€</div>
+        </div>`;
+
+        containerMovements.insertAdjacentHTML('afterbegin',html);
+        //This method is used to insert the elements inside container it takes two arguments first->where to insert ,second->what to insert
+
+         
+    });
+}
+
+displayMovements(account1.movements);
+

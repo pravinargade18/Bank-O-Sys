ce60b76b923288be99b509a3a811ffcf04b0afb3
pravinargade18 <pravinargade18@gmail.com>
2022-09-13 23:41:50 +0530

    minor changes

--- script.js
+++ script.js
@@ -7,9 +7,22 @@
 // Data
 const account1 = {
   owner: 'Jonas Schmedtmann',
-  movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
+  movements: [200, 455.23, -306.5, 25000, -642.21, -133.9, 79.97, 1300],
   interestRate: 1.2, // %
   pin: 1111,
+
+  movementsDates: [
+    '2019-11-18T21:31:17.178Z',
+    '2019-12-23T07:42:02.383Z',
+    '2020-01-28T09:15:04.904Z',
+    '2020-04-01T10:17:24.185Z',
+    '2020-05-08T14:11:59.604Z',
+    '2020-05-27T17:01:17.194Z',
+    '2020-07-11T23:36:17.929Z',
+    '2020-07-12T10:51:36.790Z',
+  ],
+  currency: 'EUR',
+  locale: 'pt-PT', // de-DE
 };
 
 const account2 = {
@@ -17,23 +30,22 @@ const account2 = {
   movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
   interestRate: 1.5,
   pin: 2222,
+
+  movementsDates: [
+    '2019-11-01T13:15:33.035Z',
+    '2019-11-30T09:48:16.867Z',
+    '2019-12-25T06:04:23.907Z',
+    '2020-01-25T14:18:46.235Z',
+    '2020-02-05T16:33:06.386Z',
+    '2020-04-10T14:43:26.374Z',
+    '2020-06-25T18:49:59.371Z',
+    '2020-07-26T12:01:20.894Z',
+  ],
+  currency: 'USD',
+  locale: 'en-US',
 };
 
-const account3 = {
-  owner: 'Steven Thomas Williams',
-  movements: [200, -200, 340, -300, -20, 50, 400, -460],
-  interestRate: 0.7,
-  pin: 3333,
-};
-
-const account4 = {
-  owner: 'Sarah Smith',
-  movements: [430, 1000, 700, 50, 90],
-  interestRate: 1,
-  pin: 4444,
-};
-
-const accounts = [account1, account2, account3, account4];
+const accounts = [account1, account2];
 
 // Elements
 const labelWelcome = document.querySelector('.welcome');
@@ -87,10 +99,11 @@ const displayMovements = function(movements, sort=false) {
     movs.forEach(function(mov,i){
       const type=mov>0?'deposit':'withdrawal';
 
-        const html = 
-        `<div class="movements__row">
-          <div class="movements__type movements__type--${type}">${i+1} ${type}</div>
-          <div class="movements__value">${mov}€</div>
+        const html = `<div class="movements__row">
+          <div class="movements__type movements__type--${type}">${
+          i + 1
+        } ${type}</div>
+          <div class="movements__value">${mov.toFixed(2)}€</div>
         </div>`;
 
         containerMovements.insertAdjacentHTML('afterbegin',html);
@@ -102,18 +115,18 @@ const displayMovements = function(movements, sort=false) {
 
 const calcDisplayBalance = function (acc) {
   acc.balance = acc.movements.reduce((acc, mov) => acc + mov, 0);
-  labelBalance.textContent = `Rs.${acc.balance}`;
+  labelBalance.textContent = `Rs.${acc.balance.toFixed(2)}`;
 };
 
 
 const calcDisplaySummary = function (acc) {
       const income=acc.movements.filter(mov=>mov>0).reduce((acc,mov)=>acc+mov,0);
-      labelSumIn.textContent=`Rs.${income}`;
+      labelSumIn.textContent = `Rs.${income.toFixed(2)}`;
 
       const out= acc.movements.filter(mov=>mov<0).reduce((acc,mov)=>acc+mov,0);
-      labelSumOut.textContent=`Rs.${Math.abs(out)}`;
+      labelSumOut.textContent = `Rs.${Math.abs(out.toFixed(2))}`;
       const interest=acc.movements.map(deposit=>(deposit*acc.interestRate)/100).filter(deposit=>deposit>=1).reduce((acc,int)=>acc+int,0)
-      labelSumInterest.textContent=`Rs.${interest}`
+      labelSumInterest.textContent=`Rs.${interest.toFixed(2)}`
 }
 
 
@@ -146,7 +159,7 @@ btnLogin.addEventListener('click', (event) =>{
   currentAccount = accounts.find(acc => acc.username === inputLoginUsername.value);
   console.log(currentAccount);
 
-  if(currentAccount?.pin===Number(inputLoginPin.value)){
+  if(currentAccount?.pin===+(inputLoginPin.value)){
     labelWelcome.textContent=`Welcome back, ${currentAccount.owner.split(' ')[0]}`;
 
     containerApp.style.opacity=100;
@@ -165,7 +178,7 @@ btnLogin.addEventListener('click', (event) =>{
 
 btnTransfer.addEventListener('click',(event)=>{
   event.preventDefault();
-  const amount = Number(inputTransferAmount.value);
+  const amount = +(inputTransferAmount.value);  //+ is used to convert the string into a number
 
   const recieversAccount = accounts.find(acc=>acc.username===inputTransferTo.value);
   inputTransferTo.value='';
@@ -183,7 +196,7 @@ btnTransfer.addEventListener('click',(event)=>{
 
 btnLoan.addEventListener('click', function(event){
     event.preventDefault();
-    const loan = Number(inputLoanAmount.value);
+    const loan = Math.floor(inputLoanAmount.value);
        
     inputLoanAmount.value = '';
 
@@ -198,7 +211,7 @@ btnLoan.addEventListener('click', function(event){
 btnClose.addEventListener('click',(event) =>{
     event.preventDefault();
 
-    if(inputCloseUsername.value===currentAccount.username && Number(inputClosePin.value)===currentAccount.pin){
+    if(inputCloseUsername.value===currentAccount.username && +(inputClosePin.value)===currentAccount.pin){
         const index = accounts.findIndex(
           acc => acc.username === currentAccount.username
         );
